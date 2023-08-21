400022d2f58cf7eca537a86f55258060d0c42bff
rudra2009 <142812542+rudra2009@users.noreply.github.com>
2023-08-21 19:47:42 +0530

    My First java Programm

---
+++ cal.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Calculater {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+        System.out.println("Enter two numbers:");
+
+        int num1 = scanner.nextInt();       
+        int num2 = scanner.nextInt();
+
+        System.out.println("Select an operation:");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+
+        int operation = scanner.nextInt();
+        double result = 0;
+
+        switch (operation) {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operation.");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+
+        scanner.close();
+
+    
+}
+}
